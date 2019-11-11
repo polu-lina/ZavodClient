25ed7fcb70ffff4e48b33dd1b346eb4134afa620
polu-lina <polupanovalina@gmail.com>
2019-11-11 05:47:33 +0500

    init

---
+++ ZavodClientSolution/ZavodClient/ClientMainClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ZavodClient
+{
+    
+    public class ClientMainClass
+    {
+        public static HttpClient Client;
+        public static string BaseUrl;
+
+        public ClientMainClass()
+        {
+            Client = new HttpClient();
+            BaseUrl = Config.BaseUrlUnits;
+        }
+
+        public async Task<Uri> CreateObject(UnitDto objectDto)
+        {
+            HttpResponseMessage response = await Client.PostAsJsonAsync(
+                BaseUrl, objectDto);
+            response.EnsureSuccessStatusCode();
+            return response.Headers.Location;
+        }
+
+        static async Task<UnitDto> GetObjectByIdAsync(Guid id)
+        {
+            HttpResponseMessage response = await Client.GetAsync(BaseUrl + id.ToString());
+            response.EnsureSuccessStatusCode();
+            UnitDto objectDto = await response.Content.ReadAsAsync<UnitDto>();
+            return objectDto;
+        }
+
+        static async Task<List<Guid>> GetObjectsIds()
+        {
+            HttpResponseMessage response = await Client.GetAsync(BaseUrl);
+            response.EnsureSuccessStatusCode();
+            List<Guid> objectsDto = await response.Content.ReadAsAsync<List<Guid>>();
+            return objectsDto;
+        }
+    }
+}
---
+++ ZavodClientSolution/ZavodClient/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZavodClient
+{
+    public class Config
+    {
+        public static string BaseUrlUnits = "http://localhost:5000/units/";
+    }
+}
